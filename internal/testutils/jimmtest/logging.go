e440d2cd123cf25b66c7238d7a614ce730d571df
SimoneDutto <simone.dutto@canonical.com>
2024-10-25 10:08:57 +0200

    set logging level for connsuite

--- internal/testutils/jimmtest/logging.go
+++ internal/testutils/jimmtest/logging.go
@@ -2,31 +2,11 @@
 package jimmtest
 
 import (
-	"time"
-
-	"github.com/juju/loggo"
 	"github.com/juju/zaputil/zapctx"
 	gc "gopkg.in/check.v1"
 
 	"github.com/canonical/jimm/v3/internal/logger"
 )
-
-// init is a workaround to reset the logger coming from the loggo library.
-// This is to prevent juju debug logs from cluttering our tests output.
-func init() {
-	ticker := time.NewTicker(100 * time.Millisecond)
-	done := make(chan bool)
-	go func() {
-		for {
-			select {
-			case <-done:
-				return
-			case <-ticker.C:
-				loggo.ResetLogging()
-			}
-		}
-	}()
-}
 
 // LoggingSuite is a replacement for github.com/juju/testing.LoggingSuite
 // zap logging but also replaces the global loggo logger.
@@ -51,5 +31,4 @@ func (s *LoggingSuite) TearDownTest(c *gc.C) {
 func (s *LoggingSuite) setUp(c *gc.C) {
 	goCheckLogger := logger.NewGoCheckLogger(c)
 	zapctx.Default = goCheckLogger
-	loggo.ResetLogging()
 }
