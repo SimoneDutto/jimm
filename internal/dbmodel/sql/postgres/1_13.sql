fc7a4b65d747bc52d43a0e181d67f2c5fe077ad4
ale8k <alexjkilroy@gmail.com>
2024-11-18 11:12:51 +0000

    feat(db): update db with roles
    Updates db with roles entries.

---
+++ internal/dbmodel/sql/postgres/1_13.sql
@@ -0,0 +1,8 @@
+-- 1_13.sql is a migration that introduces the concept of roles to JIMM.
+CREATE TABLE IF NOT EXISTS roles (
+   id BIGSERIAL PRIMARY KEY,
+   created_at TIMESTAMP WITH TIME ZONE,
+   updated_at TIMESTAMP WITH TIME ZONE,
+   name TEXT NOT NULL UNIQUE
+   uuid TEXT NOT NULL UNIQUE
+);
