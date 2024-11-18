d668095c76d8fbace195cf9657da884010eaf66b
ale8k <alexjkilroy@gmail.com>
2024-11-18 12:22:28 +0000

    feat(database): adds database methods for roles according to the specification

--- internal/db/role.go
+++ internal/db/role.go
@@ -31,3 +31,113 @@ func (d *Database) AddRole(ctx context.Context, name string) (re *dbmodel.RoleEn
 	}
 	return re, nil
 }
+
+// GetRole populates the provided *dbmodel.RoleEntry based on ID, name or UUID.
+func (d *Database) GetRole(ctx context.Context, role *dbmodel.RoleEntry) (err error) {
+	const op = errors.Op("db.GetRole")
+	if err := d.ready(); err != nil {
+		return errors.E(op, err)
+	}
+
+	durationObserver := servermon.DurationObserver(servermon.DBQueryDurationHistogram, string(op))
+	defer durationObserver()
+	defer servermon.ErrorCounter(servermon.DBQueryErrorCount, &err, string(op))
+
+	db := d.DB.WithContext(ctx)
+	if role.ID != 0 {
+		db = db.Where("id = ?", role.ID)
+	}
+	if role.UUID != "" {
+		db = db.Where("uuid = ?", role.UUID)
+	}
+	if role.Name != "" {
+		db = db.Where("name = ?", role.Name)
+	}
+	if err := db.First(&role).Error; err != nil {
+		return errors.E(op, dbError(err))
+	}
+	return nil
+}
+
+// UpdateRole updates the role identified by its ID or UUID.
+func (d *Database) UpdateRole(ctx context.Context, role *dbmodel.RoleEntry) (err error) {
+	const op = errors.Op("db.UpdateRole")
+
+	if role.ID == 0 {
+		return errors.E(errors.CodeNotFound)
+	}
+	if role.UUID == "" {
+		return errors.E("role uuid not specified", errors.CodeNotFound)
+	}
+
+	if err := d.ready(); err != nil {
+		return errors.E(op, err)
+	}
+
+	durationObserver := servermon.DurationObserver(servermon.DBQueryDurationHistogram, string(op))
+	defer durationObserver()
+	defer servermon.ErrorCounter(servermon.DBQueryErrorCount, &err, string(op))
+
+	if err := d.DB.WithContext(ctx).Save(role).Error; err != nil {
+		return errors.E(op, dbError(err))
+	}
+	return nil
+}
+
+// RemoveRole removes the role identified by its ID or UUID.
+func (d *Database) RemoveRole(ctx context.Context, role *dbmodel.RoleEntry) (err error) {
+	const op = errors.Op("db.RemoveRole")
+
+	if role.ID == 0 {
+		return errors.E(errors.CodeNotFound)
+	}
+	if role.UUID == "" {
+		return errors.E(errors.CodeNotFound)
+	}
+
+	if err := d.ready(); err != nil {
+		return errors.E(op, err)
+	}
+
+	durationObserver := servermon.DurationObserver(servermon.DBQueryDurationHistogram, string(op))
+	defer durationObserver()
+	defer servermon.ErrorCounter(servermon.DBQueryErrorCount, &err, string(op))
+
+	if err := d.DB.WithContext(ctx).Delete(role).Error; err != nil {
+		return errors.E(op, dbError(err))
+	}
+	return nil
+}
+
+// ForEachRole iterates through all role entries applying the provided callback function.
+func (d *Database) ForEachRole(ctx context.Context, f func(*dbmodel.RoleEntry) error) (err error) {
+	const op = errors.Op("db.ForEachRole")
+	if err := d.ready(); err != nil {
+		return errors.E(op, err)
+	}
+
+	durationObserver := servermon.DurationObserver(servermon.DBQueryDurationHistogram, string(op))
+	defer durationObserver()
+	defer servermon.ErrorCounter(servermon.DBQueryErrorCount, &err, string(op))
+
+	db := d.DB.WithContext(ctx).Model(&dbmodel.RoleEntry{})
+
+	rows, err := db.Rows()
+	if err != nil {
+		return errors.E(op, err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var ale dbmodel.RoleEntry
+		if err := db.ScanRows(rows, &ale); err != nil {
+			return errors.E(op, err)
+		}
+		if err := f(&ale); err != nil {
+			return err
+		}
+	}
+	if rows.Err() != nil {
+		return errors.E(op, rows.Err())
+	}
+	return nil
+}
--- internal/db/role_test.go
+++ internal/db/role_test.go
@@ -4,6 +4,7 @@ package db_test
 
 import (
 	"context"
+	"sort"
 
 	qt "github.com/frankban/quicktest"
 	"github.com/google/uuid"
@@ -42,4 +43,153 @@ func (s *dbSuite) TestDatabase_AddRole(c *qt.C) {
 	c.Assert(re.ID, qt.Equals, uint(1))
 	c.Assert(re.Name, qt.Equals, "test-role")
 	c.Assert(re.UUID, qt.Equals, uuid)
+}
+
+func (s *dbSuite) TestDatabase_GetRole(c *qt.C) {
+	uuid1 := uuid.NewString()
+	c.Patch(db.NewUUID, func() string {
+		return uuid1
+	})
+
+	err := s.Database.GetRole(context.Background(), &dbmodel.RoleEntry{
+		Name: "test-role",
+	})
+	c.Check(errors.ErrorCode(err), qt.Equals, errors.CodeUpgradeInProgress)
+
+	err = s.Database.Migrate(context.Background(), false)
+	c.Assert(err, qt.IsNil)
+
+	role := &dbmodel.RoleEntry{
+		Name: "test-role",
+	}
+	err = s.Database.GetRole(context.Background(), role)
+	c.Check(errors.ErrorCode(err), qt.Equals, errors.CodeNotFound)
+
+	roleEntry, err := s.Database.AddRole(context.TODO(), "test-role")
+	c.Assert(err, qt.IsNil)
+	c.Assert(roleEntry.UUID, qt.Equals, uuid1)
+
+	err = s.Database.GetRole(context.Background(), role)
+	c.Check(err, qt.IsNil)
+	c.Assert(role.ID, qt.Equals, uint(1))
+	c.Assert(role.Name, qt.Equals, "test-role")
+	c.Assert(role.UUID, qt.Equals, uuid1)
+
+	uuid2 := uuid.NewString()
+	c.Patch(db.NewUUID, func() string {
+		return uuid2
+	})
+
+	roleEntry, err = s.Database.AddRole(context.Background(), "test-role1")
+	c.Assert(err, qt.IsNil)
+	c.Assert(roleEntry.UUID, qt.Equals, uuid2)
+
+	role = &dbmodel.RoleEntry{
+		Name: "test-role1",
+	}
+
+	err = s.Database.GetRole(context.Background(), role)
+	c.Check(err, qt.IsNil)
+	c.Assert(role.ID, qt.Equals, uint(2))
+	c.Assert(role.Name, qt.Equals, "test-role1")
+	c.Assert(role.UUID, qt.Equals, uuid2)
+}
+
+func (s *dbSuite) TestDatabase_UpdateRole(c *qt.C) {
+	err := s.Database.UpdateRole(context.Background(), &dbmodel.RoleEntry{Name: "test-role"})
+	c.Check(errors.ErrorCode(err), qt.Equals, errors.CodeNotFound)
+
+	err = s.Database.Migrate(context.Background(), false)
+	c.Assert(err, qt.IsNil)
+
+	ge := &dbmodel.RoleEntry{
+		Name: "test-role",
+	}
+
+	err = s.Database.UpdateRole(context.Background(), ge)
+	c.Check(errors.ErrorCode(err), qt.Equals, errors.CodeNotFound)
+
+	_, err = s.Database.AddRole(context.Background(), "test-role")
+	c.Assert(err, qt.IsNil)
+
+	ge1 := &dbmodel.RoleEntry{
+		Name: "test-role",
+	}
+	err = s.Database.GetRole(context.Background(), ge1)
+	c.Assert(err, qt.IsNil)
+
+	ge1.Name = "renamed-role"
+	err = s.Database.UpdateRole(context.Background(), ge1)
+	c.Check(err, qt.IsNil)
+
+	ge2 := &dbmodel.RoleEntry{
+		Name: "renamed-role",
+	}
+	err = s.Database.GetRole(context.Background(), ge2)
+	c.Check(err, qt.IsNil)
+	c.Assert(ge2, qt.DeepEquals, ge1)
+}
+
+func (s *dbSuite) TestDatabase_RemoveRole(c *qt.C) {
+	err := s.Database.RemoveRole(context.Background(), &dbmodel.RoleEntry{Name: "test-role"})
+	c.Check(errors.ErrorCode(err), qt.Equals, errors.CodeNotFound)
+
+	err = s.Database.Migrate(context.Background(), false)
+	c.Assert(err, qt.IsNil)
+
+	ge := &dbmodel.RoleEntry{
+		Name: "test-role",
+	}
+	err = s.Database.RemoveRole(context.Background(), ge)
+	c.Check(errors.ErrorCode(err), qt.Equals, errors.CodeNotFound)
+
+	roleEntry, err := s.Database.AddRole(context.Background(), ge.Name)
+	c.Assert(err, qt.IsNil)
+
+	ge1 := &dbmodel.RoleEntry{
+		Name: "test-role",
+	}
+	err = s.Database.GetRole(context.Background(), ge1)
+	c.Assert(err, qt.IsNil)
+	c.Assert(roleEntry.UUID, qt.Equals, ge1.UUID)
+
+	err = s.Database.RemoveRole(context.Background(), ge1)
+	c.Check(err, qt.IsNil)
+
+	err = s.Database.GetRole(context.Background(), ge1)
+	c.Check(errors.ErrorCode(err), qt.Equals, errors.CodeNotFound)
+}
+
+func (s *dbSuite) TestDatabase_ForEachRole(c *qt.C) {
+	ctx := context.Background()
+
+	err := s.Database.Migrate(context.Background(), false)
+	c.Assert(err, qt.IsNil)
+
+	_, err = s.Database.AddRole(ctx, "role-1")
+	c.Assert(err, qt.IsNil)
+
+	_, err = s.Database.AddRole(ctx, "role-2")
+	c.Assert(err, qt.IsNil)
+
+	_, err = s.Database.AddRole(ctx, "role-3")
+	c.Assert(err, qt.IsNil)
+
+	var roleNames []string
+
+	err = s.Database.ForEachRole(ctx, func(re *dbmodel.RoleEntry) error {
+		roleNames = append(roleNames, re.Name)
+		return nil
+	})
+	c.Assert(err, qt.IsNil)
+
+	sort.Slice(roleNames, func(i, j int) bool {
+		return i < j
+	})
+
+	c.Assert(roleNames, qt.DeepEquals, []string{
+		"role-1",
+		"role-2",
+		"role-3",
+	})
 }
