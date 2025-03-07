48e043e10e75bc7a95c1552856b3ba96b326caea
SimoneDutto <simone.dutto@canonical.com>
2025-03-07 17:04:17 +0100

    ignore logs for some paths

--- internal/logger/http_logger.go
+++ internal/logger/http_logger.go
@@ -1,9 +1,10 @@
-// Copyright 2024 Canonical.
+// Copyright 2025 Canonical.
 
 package logger
 
 import (
 	"net/http"
+	"slices"
 	"time"
 
 	"github.com/go-chi/chi/v5/middleware"
@@ -20,6 +21,8 @@ type httpLogEntry struct {
 	request *http.Request
 }
 
+var pathsToIgnore = []string{"/", "/.well-known/jwks.json"}
+
 // Write is called when the request handler has finished.
 func (l *httpLogEntry) Write(status, bytes int, header http.Header, elapsed time.Duration, extra interface{}) {
 	fields := make([]zap.Field, 0)
@@ -27,13 +30,19 @@ func (l *httpLogEntry) Write(status, bytes int, header http.Header, elapsed time
 	if status != 0 {
 		fields = append(fields, zap.Int("status", status))
 	}
+	path := l.request.RequestURI
+	// we don't want logs for the health checks or jwks.json
+	if slices.Contains(pathsToIgnore, path) {
+		return
+	}
 	fields = append(fields,
 		zap.String("method", l.request.Method),
-		zap.String("path", l.request.RequestURI),
+		zap.String("path", path),
 		zap.String("elapsed", elapsed.String()),
 	)
 
-	if status != 200 {
+	// we log it to debug if the request is successful
+	if status == 200 {
 		zapctx.Debug(l.request.Context(), "request", fields...)
 	} else {
 		zapctx.Warn(l.request.Context(), "request", fields...)
