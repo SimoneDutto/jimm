8b508a59e3fdd4845434468d6f9e773f08a45dca
SimoneDutto <simone.dutto@canonical.com>
2026-02-18 11:16:35 +0100

    chore(e2e-test): add offer-uuid to tests because in the new juju pkg we fetch offer-uuid as well

--- testing/applicationoffers_test.go
+++ testing/applicationoffers_test.go
@@ -326,6 +326,7 @@ func (s *applicationOffersSuite) TestListApplicationOffers(c *gc.C) {
 			UserName: ofganames.EveryoneUser,
 			Access:   "read",
 		}},
+		OfferUUID: offers[0].OfferUUID,
 	}})
 }
 
@@ -528,6 +529,7 @@ func (s *applicationOffersSuite) TestFindApplicationOffers(c *gc.C) {
 			UserName: ofganames.EveryoneUser,
 			Access:   "read",
 		}},
+		OfferUUID: offers[0].OfferUUID,
 	}})
 
 	// by default each offer is publicly readable -> charlie should be
@@ -610,6 +612,7 @@ func (s *applicationOffersSuite) TestApplicationOffers(c *gc.C) {
 			UserName: ofganames.EveryoneUser,
 			Access:   "read",
 		}},
+		OfferUUID: offer.OfferUUID,
 	})
 
 	_, err = client.ApplicationOffer("charlie@canonical.com/" + s.Model.Name + ".test-offer2")
